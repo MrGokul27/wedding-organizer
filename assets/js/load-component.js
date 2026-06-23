aab134d3432394d1c54bb5f9a8aa92e7dd48351e
Gokulanath <gokulanathjp@gmail.com>
2026-06-23 15:27:17 +0530

    refactor: login changes

--- assets/js/load-component.js
+++ assets/js/load-component.js
@@ -2,20 +2,26 @@ document.addEventListener("DOMContentLoaded", async () => {
   const headerContainer = document.getElementById("header-container");
   const footerContainer = document.getElementById("footer-container");
 
+  // Check if current page is inside /pages/
+  const isInsidePages = window.location.pathname.includes("/pages/");
+
+  const headerPath = isInsidePages
+    ? "./common/header.html"
+    : "./pages/common/header.html";
+
+  const footerPath = isInsidePages
+    ? "./common/footer.html"
+    : "./pages/common/footer.html";
+
   if (headerContainer) {
-    const header = await fetch("./pages/common/header.html");
+    const header = await fetch(headerPath);
     headerContainer.innerHTML = await header.text();
 
-    // Navbar scroll effect
     const nav = document.getElementById("mainNav");
 
     if (nav) {
       const handleScroll = () => {
-        if (window.scrollY > 50) {
-          nav.classList.add("scrolled");
-        } else {
-          nav.classList.remove("scrolled");
-        }
+        nav.classList.toggle("scrolled", window.scrollY > 50);
       };
 
       handleScroll();
@@ -24,7 +30,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 
   if (footerContainer) {
-    const footer = await fetch("./pages/common/footer.html");
+    const footer = await fetch(footerPath);
     footerContainer.innerHTML = await footer.text();
   }
 });
